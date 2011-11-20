124932fb77c7cc53a70762f51ff1c7fbe0874b0b
Jack O'Connor <jackocnr@gmail.com>
2011-11-20 22:02:27 +0000

    added mobile (click) support. removed version number from dev version

--- jquery.hoverImageEnlarge.js
+++ jquery.hoverImageEnlarge.js
@@ -1,5 +1,5 @@
 /*
- * HoverImageEnlarge.js 1.0
+ * HoverImageEnlarge.js 1.1
  * Written by Jack O'Connor http://jackocnr.com
  * Copyright 2011, Bit Circus http://bitcircus.co.uk
  * Released under the WTFPL license 
@@ -9,7 +9,15 @@
 (function($) {
 	$.fn.hoverImageEnlarge = function() {
 		
+		
+		// check if browser is android/apple etc
+		var isMobile = ((navigator.userAgent.match(/iPhone/i))
+				|| (navigator.userAgent.match(/iPod/i))
+				|| (navigator.userAgent.match(/iPad/i))
+				|| (navigator.userAgent.match(/Android/i)));
+		
 		var imgBoxes = $(this).parent();
+		
 		// first save the original CSS values for use later on
 		imgBoxes.each(function() {
 			// box width/height values
@@ -25,34 +33,59 @@
 			profilePic.data("originalLeft", imgLeft);
 			//console.log("storing box width="+boxWidth+", height="+boxHeight+", img top="+imgTop+", left="+imgLeft);
 		});
-		// set hover events
-		imgBoxes.hover(function() {
-			// don't do anything if already enlarged
-			if ($(this).hasClass("enlarged")) return;
-			var profilePic = $(this).children("img");
+		
+		// enlarge the given image
+		function enlargeImage(i) {
+			var profilePic = i.children("img");
 			// grow the viewer box to the full image size
 			var imgWidth = profilePic.width();
 			var imgHeight = profilePic.height();
 			//console.log("width="+imgWidth+", height="+imgHeight);
-			$(this).addClass("enlarged").stop().animate({width: imgWidth, height: imgHeight});
+			i.addClass("enlarged").stop().animate({width: imgWidth, height: imgHeight});
 			// remove negative offsets
 			profilePic.stop().animate({top: 0, left: 0});
-		},
-		function() {
+		}
+		
+		// shrink the given image
+		function shrinkImage(i) {
 			// don't do anything if not already enlarged
-			if (!$(this).hasClass("enlarged")) return;
-			var profilePic = $(this).children("img");
+			if (!i.hasClass("enlarged")) return;
+			var profilePic = i.children("img");
 			// grab original box styles
-			var originalWidth = $(this).data("originalWidth");
-			var originalHeight = $(this).data("originalHeight");
-			$(this).stop().animate({width: originalWidth, height: originalHeight}, function() {
-				$(this).removeClass("enlarged");
+			var originalWidth = i.data("originalWidth");
+			var originalHeight = i.data("originalHeight");
+			i.stop().animate({width: originalWidth, height: originalHeight}, function() {
+				i.removeClass("enlarged");
 			});
 			// grab original img styles
 			var originalTop = profilePic.data("originalTop");
 			var originalLeft = profilePic.data("originalLeft");
 			profilePic.stop().animate({top: originalTop, left: originalLeft});
-		});
+		}
+		
+		// set different event handlers depending on browser
+		if (isMobile) {
+			// set click event
+			imgBoxes.click(function() {
+				if ($(this).hasClass("enlarged")) shrinkImage($(this));
+				else {
+					// shrink others
+					$(".enlarged").each(function() {
+						shrinkImage($(this));
+					});
+					enlargeImage($(this));
+				}
+			});
+		}
+		else {
+			// set hover events
+			imgBoxes.hover(function() {
+				if (!$(this).hasClass("enlarged")) enlargeImage($(this));
+			},
+			function() {
+				if ($(this).hasClass("enlarged")) shrinkImage($(this));
+			});
+		}
 		
 		
 		
